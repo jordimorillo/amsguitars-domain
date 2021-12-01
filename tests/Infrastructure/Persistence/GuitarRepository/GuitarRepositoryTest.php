62f3fc910eafc03e52ee0a460a6da70b46c093ee
Jordi Morillo <jordi.morillo@ext.veepee.com>
2021-12-01 12:38:52 +0100

    Replace dataprovider in memory

--- tests/Infrastructure/Persistence/GuitarRepository/GuitarRepositoryTest.php
+++ tests/Infrastructure/Persistence/GuitarRepository/GuitarRepositoryTest.php
@@ -4,91 +4,84 @@ declare(strict_types=1);
 
 namespace Tests\Infrastructure\Persistence\GuitarRepository;
 
+use AMSGuitars\Domain\Entities\Guitar\GuitarRepositoryInterface;
 use AMSGuitars\Domain\ValueObjects\Collections\GuitarCollection;
 use AMSGuitars\Domain\ValueObjects\Identifiers\GuitarId;
 use AMSGuitars\Infrastructure\Exceptions\GuitarNotFound;
+use AMSGuitars\Infrastructure\Persistence\GuitarRepository\GuitarRepositoryInMemory;
 use AMSGuitars\Infrastructure\ValueObjects\Persistence\Column;
 use AMSGuitars\Infrastructure\ValueObjects\Persistence\Limit;
-use AMSGuitars\Infrastructure\ValueObjects\Persistence\SortOrder;
-use AMSGuitars\Infrastructure\Persistence\GuitarRepository\GuitarRepositoryInMemory;
-use AMSGuitars\Domain\Entities\Guitar\GuitarRepositoryInterface;
 use AMSGuitars\Infrastructure\ValueObjects\Persistence\SortDirection;
-use PHPUnit\Framework\TestCase;
+use AMSGuitars\Infrastructure\ValueObjects\Persistence\SortOrder;
+use Tests\DatabaseTestCase;
 use Tests\Fixtures\Domain\Guitars;
 
-class GuitarRepositoryTest extends TestCase
+class GuitarRepositoryTest extends DatabaseTestCase
 {
     public function dataProvider(): array
     {
         return [
-            'In memory' => [GuitarRepositoryInMemory::class],
+            'In memory' => [new GuitarRepositoryInMemory],
         ];
     }
 
     /** @dataProvider dataProvider() */
-    public function testCanInstantiate(string $guitarRepositoryClass): void
+    public function testCanInstantiate(GuitarRepositoryInterface $guitarRepository): void
     {
-        /** @var GuitarRepositoryInterface $guitarRepository */
-        $guitarRepository = new $guitarRepositoryClass();
         self::assertInstanceOf(GuitarRepositoryInterface::class, $guitarRepository);
     }
 
     /** @dataProvider dataProvider() */
-    public function testCanSave(string $guitarRepositoryClass): void
+    public function testCanSave(GuitarRepositoryInterface $guitarRepository): void
     {
         $aGuitar = (new Guitars())->aGuitar();
-        /** @var GuitarRepositoryInterface $guitarRepository */
-        $guitarRepository = new $guitarRepositoryClass();
         $guitarRepository->save($aGuitar);
         self::assertTrue(true);
     }
 
     /** @dataProvider dataProvider() */
-    public function testCanFindById(string $guitarRepositoryClass): void
+    public function testCanFindById(GuitarRepositoryInterface $guitarRepository): void
     {
         $aGuitar = (new Guitars())->aGuitar();
         $guitarCollection = new GuitarCollection([$aGuitar]);
-        /** @var GuitarRepositoryInterface $guitarRepository */
-        $guitarRepository = new $guitarRepositoryClass($guitarCollection);
+        foreach($guitarCollection->getIterator() as $guitar) {
+            $guitarRepository->save($guitar);
+        }
         $obtainedGuitar = $guitarRepository->findById($aGuitar->getGuitarId());
         self::assertEquals($aGuitar, $obtainedGuitar);
     }
 
     /** @dataProvider dataProvider() */
-    public function testCanThrowExceptionIfCantFindById(string $guitarRepositoryClass): void
+    public function testCanThrowExceptionIfCantFindById(GuitarRepositoryInterface $guitarRepository): void
     {
-        /** @var GuitarRepositoryInterface $guitarRepository */
-        $guitarRepository = new $guitarRepositoryClass();
         $this->expectException(GuitarNotFound::class);
         $guitarRepository->findById(new GuitarId());
     }
 
     /** @dataProvider dataProvider() */
-    public function testCanFindACollection(string $guitarRepositoryClass): void
+    public function testCanFindACollection(GuitarRepositoryInterface $guitarRepository): void
     {
         $guitarCollection = new GuitarCollection();
         for ($i = 0; $i <= 10; $i++) {
             $aGuitar = (new Guitars())->aGuitar();
             $guitarCollection->add($aGuitar);
+            $guitarRepository->save($aGuitar);
         }
         $findOrder = new SortOrder(new Column('guitarId'));
         $guitarCollection->sort($findOrder->getColumn()->toString());
-        /** @var GuitarRepositoryInterface $guitarRepository */
-        $guitarRepository = new $guitarRepositoryClass($guitarCollection);
         $obtainedCollection = $guitarRepository->findCollection($findOrder);
         self::assertEquals($guitarCollection, $obtainedCollection);
     }
 
     /** @dataProvider dataProvider() */
-    public function testCanFindACollectionWithLimit(string $guitarRepositoryClass): void
+    public function testCanFindACollectionWithLimit(GuitarRepositoryInterface $guitarRepository): void
     {
         $guitarCollection = new GuitarCollection();
         for ($i = 1; $i <= 10; $i++) {
             $aGuitar = (new Guitars())->aGuitar();
             $guitarCollection->add($aGuitar);
+            $guitarRepository->save($aGuitar);
         }
-        /** @var GuitarRepositoryInterface $guitarRepository */
-        $guitarRepository = new $guitarRepositoryClass($guitarCollection);
         $findLimit = new Limit(0, 2);
         $findOrder = new SortOrder(new Column('guitarId'), SortDirection::ASC());
         $guitarCollection->sort($findOrder->getColumn()->toString());
